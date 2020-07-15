a656e2e78342d0079224a828b5ac7438016f748b
Shriniket Deshmukh <33655616+shriDeveloper@users.noreply.github.com>
2020-07-15 22:50:10 +0530

    Update views.py

--- search/views.py
+++ search/views.py
@@ -1,17 +1,16 @@
 import youtube_dl
 import os
 import re
-from youtube_search import YoutubeSearch
-from django.http import HttpResponse,Http404,JsonResponse
+import uuid
+from django.http import HttpResponse,Http404
 from django.views.decorators.csrf import csrf_exempt
 from django.conf import settings
 from django.shortcuts import render,redirect
 def search(request):
-    query=request.GET.get("song_id")
+    query=request.POST.get("term")
     if query is None:
         return redirect('/')
-    song_name=request.GET.get('short_hand')
-    result=request.GET.get('unique_hash')
+    result=str(uuid.uuid1())+'.mp3'
     ydl_opts = {
     'format': 'worstaudio/worst',
     'postprocessors': [{'key': 'FFmpegExtractAudio',
@@ -21,31 +20,14 @@ def search(request):
     'noplaylist':True,
     'ignoreerrors':True,
     'max-downloads':1,
-    'external-downloader':'aria2c',
-    'external-downloader-args':'-c -j 3 -x 3 -s 3 -k 1M',
     }
     ydl = youtube_dl.YoutubeDL(ydl_opts)
-    try:
-        ydl.download([query])
-    except  youtube_dl.utils.DownloadError:
-        return render(request,'homepage/index_new.html',{'ERROR':"This Songs is not available in your country."})
-#     return redirect('ec2-18-223-136-205.us-east-2.compute.amazonaws.com/media/'+result,content_type="application/vnd.ms-excel")
+    info_dict = ydl.extract_info(query, download=True)
+    title=re.sub('[\W_]+','',str(info_dict['title']),flags=re.ASCII)
     file_path = os.path.join(settings.MEDIA_ROOT, result)
     if os.path.exists(file_path):
         with open(file_path, 'rb') as fh:
-            response = HttpResponse("ec2-18-223-136-205.us-east-2.compute.amazonaws.com/media/"+fh.read(), content_type="application/vnd.ms-excel")
-            response['Content-Disposition'] = 'inline; filename=' + song_name
+            response = HttpResponse(fh.read(), content_type="application/vnd.ms-excel")
+            response['Content-Disposition'] = 'inline; filename='+title+'.mp3'
             return response
     raise Http404
-@csrf_exempt
-def autosuggest(request):
-    data = request.GET.get("term")
-    if data is None:
-        return redirect('/')
-    results=[]
-    while not results:
-        results = YoutubeSearch(''+data, max_results=5).to_dict()
-    for result in results:
-        title=re.sub('[\W_]+','',str(result['title']),flags=re.ASCII)
-        result['short_hand']=title+".mp3"
-    return JsonResponse({"results":results},safe=False)
